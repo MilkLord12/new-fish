8a7580e78d60540143e7ec83c1d8fc22592a22af
MilkLord12 <163229108+MilkLord12@users.noreply.github.com>
2024-03-13 22:51:29 -0400

    Add files via upload
    update

---
+++ new-fish-main/Fish-main/main/background.go
@@ -0,0 +1,28 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"New Animation\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 660.0
+    y: -302.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 4.0
+    y: 5.0
+    z: 1.0
+  }
+}
