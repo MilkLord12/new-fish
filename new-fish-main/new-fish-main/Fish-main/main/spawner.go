1be8fa2c9c5d50b8e8a550bcbf3a702b25725da2
MilkLord12 <163229108+MilkLord12@users.noreply.github.com>
2024-03-15 12:45:37 -0400

    Add files via upload

---
+++ new-fish-main/new-fish-main/Fish-main/main/spawner.go
@@ -0,0 +1,34 @@
+components {
+  id: "spawner"
+  component: "/main/spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factori"
+  type: "factory"
+  data: "prototype: \"/main/enemy.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
